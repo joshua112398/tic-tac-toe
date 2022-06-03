8ccf1c598cb16e1aaeb8f1f63b449b572dc9d270
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-06-03 00:26:03 -0700

    Implement algorithm to check if a player won.
    Create an object of all eight possible winning patterns, and check
    if either player has the pattern existing on the board, and declare
    them the winner.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 // GAMEBOARD MODULE
 const Gameboard = (function() {
-  const squares = ["", "", "", "", "", "", "", "", ""];
+  const _squares = ["", "", "", "", "", "", "", "", ""];
 
   // Display the updated board
   const displayBoard = function() {
     const board = document.querySelector(".board");
 
     let i = 0;
-    for (const square of squares) {
+    for (const square of _squares) {
       const div = document.createElement("div");
       div.classList.add("square");
       div.setAttribute("data-index", i)
@@ -42,25 +42,32 @@ const Gameboard = (function() {
 
   // Edit one square on the board
   const markBoard = function(index, marker) {
-    if (squares[index] !== "") {
+    if (_squares[index] !== "") {
       // run invalid square function
       return;
     }
-    squares[index] = marker;
+    _squares[index] = marker;
     eraseBoard();
     displayBoard();
   }
 
-  return {squares, displayBoard, markBoard, eraseBoard};
+  // Get function for the board, to determine winner
+  const getBoard = function() {
+    return _squares;
+  }
+
+  return {displayBoard, markBoard, eraseBoard, getBoard};
 
 })();
 
 // ------------------------------------------ //
+// ------------------------------------------ //
 
 // PLAYER FACTORY FUNCTION
-const Player = function(marker, turn) {
+const Player = function(marker, turn, playerName) {
   // Sets initial score, and which marker the player uses
   const _marker = marker;
+  let _name = playerName;
   let _turn = turn;
 
   // Make a move, and mark the board using markBoard function
@@ -69,11 +76,15 @@ const Player = function(marker, turn) {
       Gameboard.markBoard(index, _marker)
     }
     _turn = !_turn;  // finish the player's turn
+    if (gameController.checkWinner(_marker)) { // then check if player won
+
+    }
   }
 
   return {makeMove};
 };
 
+// ------------------------------------------ //
 // ------------------------------------------ //
 
 // GAME CONTROL FLOW MODULE
@@ -82,6 +93,20 @@ const gameController = (function () {
   let _playerOne = Player("X", true);
   let _playerTwo = Player("O", false);
 
+  // object containing all 8 win patterns
+  // here we can see that the corner squares have 3 possible
+  // winning patterns, the side squares have 2 patterns, and the
+  /// middle square the most with 4, useful for smart AI later
+  const _winningPatterns = {
+    a: [0,1,2],
+    b: [0,4,8],
+    c: [0,3,6],
+    d: [1,4,7],
+    e: [2,5,8],
+    f: [2,4,6],
+    g: [3,4,5],
+    h: [6,7,8],
+  }
 
   const startGame = function() {
     // Starts the game, spawning the board and players
@@ -94,7 +119,28 @@ const gameController = (function () {
     _playerTwo.makeMove(index);
   }
 
-  return {startGame, playerMove};
+  const checkWinner = function(marker) {
+    const squares = Gameboard.getBoard();
+
+    for (let pattern in _winningPatterns) {
+      let winIndicator = true;
+      // for each pattern containing array indices, check the
+      // indices and see if all three have the same marker, if so,
+      // then there is a winning pattern
+      for (const index of _winningPatterns[pattern]) {
+        if (squares[index] !== marker) {
+          winIndicator = false;
+          break;
+        }
+      }
+      if (winIndicator === true) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  return {startGame, playerMove, checkWinner};
 })();
 
 
